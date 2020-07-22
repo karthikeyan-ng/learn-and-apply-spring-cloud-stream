9f23b889fe06909f9ff7a835b0485ed0caab3438
Karthikeyan Nithiyanandam <karthikeyan.ng@gmail.com>
2020-07-22 21:26:57 +0530

    Created Spring Boot Project

---
+++ message-consumer-rabbitmq/src/test/java/com/techstack/message/MessageConsumerRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techstack.message;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageConsumerRabbitmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
